c65560e1f866990e7d98be561584c7daf8b6be75
Dan Knox <dknox@threedotloft.com>
2012-06-18 12:23:14 -0700

    Add namespace support to EventMap

--- lib/websocket_rails/event_map.rb
+++ lib/websocket_rails/event_map.rb
@@ -10,6 +10,10 @@ module WebsocketRails
   #   WebsocketRails::EventMap.describe do
   #     subscribe :client_connected, to: ChatController, with_method: :client_connected
   #     subscribe :new_user, to: ChatController, with_method: :new_user
+  #
+  #     namespace :email do
+  #       subscribe :new_email, to: EmailController, with_method: :new_email
+  #     end
   #   end
   class EventMap
     
@@ -18,35 +22,70 @@ module WebsocketRails
     end
     
     attr_reader :classes, :events
+
+    GLOBAL_NAMESPACE = :global
+
+    attr_reader :global_namespace
+
+    attr_reader :current_namespace
     
     def initialize(dispatcher)
       @dispatcher = dispatcher
-      evaluate( WebsocketRails.route_block ) if WebsocketRails.route_block
+      evaluate WebsocketRails.route_block if WebsocketRails.route_block
     end
     
-    def routes_for(event,&block)
-      @events[event].each do |klass,method|
+    def routes_for(event, namespace = :global, &block)
+      events[namespace][event].each do |klass,method|
         controller = @classes[klass]
-        block.call( controller, method )
+        block.call controller, method
       end
     end
     
     def subscribe(event_name,options)
       klass  = options[:to] || raise("Must specify a class for to: option in event route")
       method = options[:with_method] || raise("Must specify a method for with_method: option in event route")
-      controller = klass.new
-      if @classes[klass].nil?
-        @classes[klass] = controller
+      if classes[klass].nil?
+        controller     = klass.new
+        classes[klass] = controller
         controller.instance_variable_set(:@_dispatcher,@dispatcher)
         controller.send :initialize_session if controller.respond_to?(:initialize_session)
       end
-      @events[event_name] << [klass,method]
-    end    
+      events[current_namespace][event_name] << [klass,method]
+    end
+
+    def namespace(new_namespace,&block)
+      @current_namespace = new_namespace
+      instance_eval &block if block.present?
+      @current_namespace = global_namespace
+    end
     
     def evaluate(block)
-      @events  = Hash.new {|h,k| h[k] = Array.new}
+      @events  = Hash.new { |hash,namespace| hash[namespace] = EventMap::Events.new }
       @classes = Hash.new
-      instance_eval &block
+      @global_namespace  = GLOBAL_NAMESPACE
+      @current_namespace = global_namespace
+      instance_eval &block if block.present?
+    end
+
+    # Thin Hash wrapper for storing events underneath different namespaces.
+    class Events
+
+      def initialize
+        @events = Hash.new {|h,k| h[k] = Array.new}
+      end
+
+      def [](k)
+        @events[k]
+      end
+
+      def []=(k,v)
+        @events[k] = v
+      end
+
+      def has_key?(k)
+        @events.has_key? k
+      end
+
     end
     
   end
--- spec/unit/event_map_spec.rb
+++ spec/unit/event_map_spec.rb
@@ -13,7 +13,7 @@ module WebsocketRails
     let(:dispatcher) { double('dispatcher').as_null_object }
     subject { EventMap.new(dispatcher) }
     
-    context "EventMap.describe" do      
+    context "EventMap.describe" do
       it "should store the event route block in the global configuration" do
         define_test_events
         WebsocketRails.route_block.should be_present
@@ -28,18 +28,27 @@ module WebsocketRails
         subject.evaluate( block )
       end
       
-      it "should initialize empty hashes for classes and events" do
+      it "should initialize an empty hash for classes" do
         subject.evaluate Proc.new {}
         subject.classes.should == {}
-        subject.events.should == {}
+      end
+
+      it "should initialize a hash primed with new EventMap::Events instances for namespaced events" do
+        subject.evaluate Proc.new {}
+        subject.events[:global].class.should == EventMap::Events
+      end
+
+      it "should set the current namespace to :global" do
+        subject.evaluate Proc.new {}
+        subject.current_namespace.should == :global
       end
     end
     
     context "#subscribe" do
       before(:each) { define_test_events }
       
-      it "should store the event in the events hash" do
-        subject.events.has_key?(:client_connected).should be_true
+      it "should store the event in the correct namespace" do
+        subject.events[:global].has_key?(:client_connected).should be_true
       end
       
       it "should store the instantiated controller in the classes hash" do
@@ -51,18 +60,49 @@ module WebsocketRails
       end
       
       it "should store the class constant and method name in the events hash" do
-        subject.events[:client_connected].should == [[Object,:object_id]]
+        subject.events[:global][:client_connected].should == [[Object,:object_id]]
+      end
+    end
+
+    context "#namespace" do
+      it "should set the current_namespace before evaluating the block" do
+        subject.namespace :scoped do
+          current_namespace.should == :scoped
+        end
+      end
+
+      it "should restore the global namespace after evaluating the block" do
+        subject.namespace :scoped do
+          true
+        end
+        subject.current_namespace.should == subject.global_namespace
+      end
+
+      it "should store subscribed events in the correct namespace" do
+        subject.namespace :scoped do
+          subscribe :new_event, to: ChatController, with_method: :scoped_chat
+        end
+        subject.events[:scoped][:new_event].should == [[ChatController,:scoped_chat]]
       end
     end
     
     context "#routes_for" do
       before(:each) { define_test_events }
       
-      it "should yield the class constant and method symbol for each route defined for an event" do
+      it "should yield the instantiated controller and action name for each route defined for an event" do
         subject.routes_for(:client_connected) do |controller,method|
           controller.class.should == Object
           method.should == :object_id
         end
+      end
+    end
+
+    describe EventMap::Events do
+
+      subject { EventMap::Events.new }
+      
+      it "should provide access to a Hash primed with new instances of Array" do
+        subject[:test].class.should == Array
       end
     end
     
