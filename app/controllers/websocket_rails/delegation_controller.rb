4c1a5d831985c3ed814641ab2bb9a5db3244c124
Dan Knox <dknox@threedotloft.com>
2012-07-02 12:09:52 -0700

    Add a description of DelegationController to the comments.

--- app/controllers/websocket_rails/delegation_controller.rb
+++ app/controllers/websocket_rails/delegation_controller.rb
@@ -1,3 +1,13 @@
-class WebsocketRails::DelegationController < ApplicationController
-
+module WebsocketRails
+  # This class provides a means for accessing the Rails
+  # controller helper methods defined in a user's application
+  # or in gems that the user has added to the project.
+  #
+  # Each active connection creates and stores it's own
+  # instance with the correct @_request and @_env objects
+  # set. WebsocketRails::BaseController sends missing
+  # methods to the active connection's delegation controller
+  # instance.
+  class DelegationController < ApplicationController
+  end
 end
