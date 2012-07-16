e016e6a702e1b811011650c86f23cb5cd4754c9a
Dan Knox <dknox@threedotloft.com>
2012-07-16 09:49:36 -0700

    Explicitly set the Rails controller load path.

--- lib/websocket_rails/engine.rb
+++ lib/websocket_rails/engine.rb
@@ -2,6 +2,7 @@ module WebsocketRails
   
   class Engine < Rails::Engine
     initializer "websocket_rails.load_app_instance_data" do |app|
+      paths['app/controllers'] = 'app/controllers'
       WebsocketRails.setup do |config|
         config.app_root = app.root
       end
