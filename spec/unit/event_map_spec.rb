8e06eff258f78c356f28577b7e6fe2a4fbac683b
Dan Knox <dknox@threedotloft.com>
2013-02-10 21:17:38 -0800

    Remove excess whitespace in event_map_spec.rb

--- spec/unit/event_map_spec.rb
+++ spec/unit/event_map_spec.rb
@@ -38,7 +38,7 @@ module WebsocketRails
 
       end
     end
-    
+
     let(:dispatcher) { double('dispatcher').as_null_object }
     subject { EventMap.new(dispatcher) }
     before { define_test_events }
@@ -48,22 +48,22 @@ module WebsocketRails
         WebsocketRails.route_block.should be_present
       end
     end
-    
+
     context "Events in the global namespace" do
-      
+
       it "should store the event in the correct namespace" do
         subject.namespace.actions[:client_connected].should be_present
         subject.namespace.name.should == :global
       end
-      
+
       it "should store the instantiated controller in the classes hash" do
         subject.namespace.controllers[ChatController].class.should == ChatController
       end
-      
+
       it "should set the dispatcher on the instantiated controller" do
         subject.namespace.controllers[ChatController].instance_variable_get(:@_dispatcher).should == dispatcher
       end
-      
+
       it "should store the class constant and method name in the events hash" do
         subject.namespace.actions[:client_connected].should == [[ChatController,:new_user]]
       end
@@ -73,18 +73,18 @@ module WebsocketRails
     context "Events in a child namespace" do
 
       before { @namespace = subject.namespace }
-      
+
       it "should store the event in the correct namespaces" do
         @namespace.namespaces[:product].actions[:update].should be_present
         @namespace.namespaces[:complex_product].actions[:simplify].should be_present
       end
-      
+
     end
 
     context "#routes_for" do
 
       let(:event) { double('event') }
-      
+
       context "with events in the global namespace" do
         it "should yield the controller and action name for each route defined for an event" do
           event.stub(:name).and_return(:client_connected)
@@ -101,7 +101,7 @@ module WebsocketRails
         it "should yield the controller and action name for each route defined with a hash for an event" do
           ProductController.any_instance.should_receive(:action_executed)
           event = HelperMethods::MockEvent.new :update, [:global,:product]
-          
+
           subject.routes_for(event) do |controller,method|
             controller.action_executed
             controller.class.should == ProductController
