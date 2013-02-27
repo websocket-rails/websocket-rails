e95cb8ea10cb4604a6f926eb5411fbae18c1befc
Dan Knox <dknox@threedotloft.com>
2013-02-27 09:11:29 -0800

    Add the updated config options to the generator.

--- lib/generators/websocket_rails/install/templates/events.rb
+++ lib/generators/websocket_rails/install/templates/events.rb
@@ -1,21 +1,31 @@
 WebsocketRails.setup do |config|
 
-  # Change to :debug for debugging output
-  config.log_level = :default
+  # Uncomment to override the default log level. The log level can be
+  # any of the standard Logger log levels. By default it will mirror the
+  # current Rails environment log level.
+  # config.log_level = :debug
+
+  # Uncomment to change the default log file path.
+  # config.log_path = "#{Rails.root}/log/websocket_rails.log"
+  
+  # Set to true if you wish to log the internal websocket_rails events
+  # such as the keepalive `websocket_rails.ping` event.
+  # config.log_internal_events = false
 
   # Change to true to enable standalone server mode
   # Start the standalone server with rake websocket_rails:start_server
-  # Requires Redis
+  # * Requires Redis
   config.standalone = false
 
   # Change to true to enable channel synchronization between
-  # multiple server instances. Requires Redis.
+  # multiple server instances.
+  # * Requires Redis.
   config.synchronize = false
 
   # Uncomment and edit to point to a different redis instance.
   # Will not be used unless standalone or synchronization mode
   # is enabled.
-  #config.redis_options = {:host => 'localhost', :port => '6379'}
+  # config.redis_options = {:host => 'localhost', :port => '6379'}
 end
 
 WebsocketRails::EventMap.describe do
