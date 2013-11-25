313965de73d92bba2dbb2f6112fe846be50837c5
Dan Knox <dknox@threedotloft.com>
2013-11-24 22:00:00 -0800

    Remove deprecated POST /websocket route.

--- lib/rails/config/routes.rb
+++ lib/rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   if Rails.version >= '4.0.0'
-    match "/websocket", :to => WebsocketRails::ConnectionManager.new, via: [:get, :post]
+    get "/websocket", :to => WebsocketRails::ConnectionManager.new
   else
-    match "/websocket", :to => WebsocketRails::ConnectionManager.new
+    get "/websocket", :to => WebsocketRails::ConnectionManager.new
   end
 end
