e3c46f1d0315b2832ad180fb4c6c30e577c4271f
totzYuta <yuta.totz@gmail.com>
2015-12-03 09:26:22 +0900

    Remove whitespace in the end of line in template of websocket_rails.rb

--- lib/generators/websocket_rails/install/templates/websocket_rails.rb
+++ lib/generators/websocket_rails/install/templates/websocket_rails.rb
@@ -37,7 +37,7 @@ WebsocketRails.setup do |config|
   # config.keep_subscribers_when_private = false
 
   # Set to true if you wish to broadcast channel subscriber_join and
-  # subscriber_part events. All subscribers of a channel will be 
+  # subscriber_part events. All subscribers of a channel will be
   # notified when other clients join and part the channel. If you are
   # using the UserManager, the current_user object will be sent along
   # with the event.
